003d4c7882a919fe190d43673b4780ca6cc070e6
sagar <sagars1.ics@gmail.com>
2019-04-05 11:16:56 +0530

    Project created and 2 screens added ie. log-in screen and tables activity

---
+++ app/src/main/java/com/resmenu/Activity/TablesActivity.java
@@ -0,0 +1,15 @@
+package com.resmenu.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.resmenu.R;
+
+public class TablesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tables);
+    }
+}
