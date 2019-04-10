d7aa65b7fed01328db3f0996544420873cb9d463
rahul <rahulbibave@gmail.com>
2019-04-10 12:15:30 +0530

    tableview

--- app/src/main/java/com/resmenu/activity/TablesActivity.java
+++ app/src/main/java/com/resmenu/activity/TablesActivity.java
@@ -2,14 +2,26 @@ package com.resmenu.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.resmenu.R;
+import com.resmenu.adapters.LiveTableAdapter;
 
 public class TablesActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerViewTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tables);
+        init();
+    }
+
+    private void init() {
+        mRecyclerViewTable=findViewById(R.id.recycler_view_live_tables);
+        mRecyclerViewTable.setLayoutManager(new GridLayoutManager(this,4));
+        LiveTableAdapter liveTableAdapter=new LiveTableAdapter(this);
+        mRecyclerViewTable.setAdapter(liveTableAdapter);
     }
 }
--- app/src/main/java/com/resmenu/adapters/LiveTableAdapter.java
+++ app/src/main/java/com/resmenu/adapters/LiveTableAdapter.java
@@ -8,35 +8,44 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.resmenu.R;
 
 import java.util.zip.Inflater;
 
 public class LiveTableAdapter extends RecyclerView.Adapter<LiveTableAdapter.TableViewHolder> {
-    private Context mContext;
+    Context mContext;
 
+    public LiveTableAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
 
     @NonNull
     @Override
-    public LiveTableAdapter.TableViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewtype) {
-        View itemView = LayoutInflater.from(mContext).inflate(R.layout.layout_table_item,parent,false);
-        return new TableViewHolder(itemView);
+    public TableViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view=LayoutInflater.from(mContext).inflate(R.layout.layout_table_item,viewGroup,false);
+        return new TableViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LiveTableAdapter.TableViewHolder tableViewHolder, int i) {
+    public void onBindViewHolder(@NonNull TableViewHolder tableViewHolder, int i) {
+        int x=i+1;
+        tableViewHolder.txtTableNo.setText(""+x);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 12;
     }
 
+
     public class TableViewHolder extends RecyclerView.ViewHolder {
+        TextView txtTableNo;
         public TableViewHolder(@NonNull View itemView) {
             super(itemView);
+            txtTableNo=itemView.findViewById(R.id.txtTableNo);
         }
     }
 }
