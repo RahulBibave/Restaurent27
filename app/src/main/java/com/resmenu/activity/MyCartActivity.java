1d8df7fec7b450845319247e0afa589bc6fe85ca
sagar <sagars1.ics@gmail.com>
2019-04-23 19:21:34 +0530

    Layout is modified buttons are added in MyCart

--- app/src/main/java/com/resmenu/activity/MyCartActivity.java
+++ app/src/main/java/com/resmenu/activity/MyCartActivity.java
@@ -1,18 +1,52 @@
 package com.resmenu.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.resmenu.Database.Entity.MyCart;
+import com.resmenu.Database.RestaurentMenuDatabase;
 import com.resmenu.R;
+import com.resmenu.adapters.MyCartAdapter;
+import com.resmenu.customViews.CustomButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyCartActivity extends AppCompatActivity {
 
+    RecyclerView mRecyclerViewCart;
+    MyCartAdapter myCartAdapter;
+    List<MyCart> myCartArrayList;
 
-
+    CustomButton mBtnPproceedtopay;
+    CustomButton mBtnContinueorde;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_cart);
+
+        mBtnPproceedtopay = findViewById(R.id.btn_proceedtopay);
+        mBtnContinueorde = findViewById(R.id.btn_continueorder);
+
+        mRecyclerViewCart = findViewById(R.id.recycler_virw_cart);
+        myCartArrayList = new ArrayList<>();
+
+        getDatabaseList();
+
+//
+    }
+
+    private void getDatabaseList() {
+
+        RestaurentMenuDatabase restaurentMenuDatabase = RestaurentMenuDatabase.getInstance(this);
+        myCartArrayList = restaurentMenuDatabase.myCartDao().getAll();
+
+        myCartAdapter = new MyCartAdapter(this ,myCartArrayList);
+        mRecyclerViewCart.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerViewCart.setAdapter(myCartAdapter);
     }
 }
--- app/src/main/java/com/resmenu/adapters/MyCartAdapter.java
+++ app/src/main/java/com/resmenu/adapters/MyCartAdapter.java
@@ -6,12 +6,24 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.resmenu.Database.Entity.MyCart;
 import com.resmenu.R;
+import com.resmenu.customViews.CustomTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyCartAdapter extends RecyclerView.Adapter<MyCartAdapter.ViewHolderMyCart> {
 
     Context mContext;
+    List<MyCart> myCartArrayList;
+
+    public MyCartAdapter(Context mContext, List<MyCart> myCartArrayList) {
+        this.mContext = mContext;
+        this.myCartArrayList = myCartArrayList;
+    }
 
     @NonNull
     @Override
@@ -21,20 +33,30 @@ public class MyCartAdapter extends RecyclerView.Adapter<MyCartAdapter.ViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderMyCart viewHolderMyCart, int i) {
+    public void onBindViewHolder(@NonNull ViewHolderMyCart holder, int i) {
 
 
+        holder.mItemName.setText(myCartArrayList.get(i).getMenuName());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return myCartArrayList.size();
     }
 
     public class ViewHolderMyCart extends RecyclerView.ViewHolder {
+
+        CustomTextView mItemName , mMinus , mPlus , mQuantity , mTotal;
+        ImageView mImgCancle;
         public ViewHolderMyCart(@NonNull View itemView) {
             super(itemView);
+            mItemName = itemView.findViewById(R.id.txt_item_name);
+            mMinus = itemView.findViewById(R.id.textViewminus);
+            mPlus = itemView.findViewById(R.id.textViewplus);
+            mQuantity = itemView.findViewById(R.id.quantity);
+            mTotal = itemView.findViewById(R.id.total);
+            mImgCancle = itemView.findViewById(R.id.imageView_cancle);
         }
     }
 }
